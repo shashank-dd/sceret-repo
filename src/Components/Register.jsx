85bdac90575e0953c8360bb91d2ab151bc3fc628
shashank-dd <shashankdd1805@gmail.com>
2022-12-24 11:04:39 +0530

    added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -5,51 +5,55 @@ import { Link, useNavigate } from "react-router-dom";
 import './Register.css'
 
 let Register = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     let [user, setusers] = useState({ email: "", password: "", confirmpassword: "" })
-    let [formerrors, setformerrors] = useState(0)
-function submithandler(e) {
+
+    function submithandler(e) {
         console.log("1")
-        setformerrors(validate(user))
-     console.log(user)
-         axios.post("https://backendreal.onrender.com/register/register",user).then(response =>{
+       
+        console.log(user)
+        if( validate(user)){
+            axios.post("https://backendreal.onrender.com/register/register", user).then(response => {
                 console.log(response.data.status)
-                if(response.data.status==="ok"){
+                if (response.data.status === "ok") {
+                    alert("registered succesfully")
                     navigate("/")
-                            // window.location.href="/"
+                    // window.location.href="/"
                 }
-        }).catch(error =>{console.log(error)})
+            }).catch(error => { console.log(error) })
         }
- let validate = (values) => {
-if (!values.email) {
+       
+    }
+    let validate = (values) => {
+        if (!values.email) {
             alert('email is required')
-            return 1
-        }
-        if (!values.password) {
+            return 0
+        }else if (!values.password) {
             alert('password is required')
-            return 1
+            return 0
         } else if (values.password.length < 4) {
             alert("Password must be more than 4 characters");
-            return 1
+            return 0
         } else if (values.password.length > 10) {
             alert("Password cannot exceed more than 10 characters");
-            return 1
+            return 0
         }
         if (!values.confirmpassword) {
             alert('confirmpassword is required')
-            return 1
+            return 0
         }
         else if (values.password !== values.confirmpassword) {
             alert('password are not matching')
-            return 1
+            return 0
         }
+        return 1
     }
     return (
         <div className="card-containers">
-           <div className="registers">
+            <div className="registers">
                 <div>
                     <h1 className="logos">Logo</h1>
-                    </div>
+                </div>
                 <div className="creates">Create New Account </div>
                 <div>
                     <input type="email" placeholder="MaildID" onChange={(e) => { setusers({ ...user, email: e.target.value }) }} />
@@ -62,10 +66,10 @@ if (!values.email) {
                 </div>
                 <button className="buttons" onClick={submithandler} >Signup</button>
             </div>
-       <div className="child-div">
+            <div className="child-div">
                 <Link to="/"><p>signIn</p></Link>
             </div>
-          </div>
+        </div>
     )
 }
 export default Register
